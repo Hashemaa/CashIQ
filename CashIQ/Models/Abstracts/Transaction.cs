bf7a600563b51702a26f64fd619d1816896d6bdf
Hashem Abou Ahmad <hashemaa.ca@gmail.com>
2025-10-04 22:04:18 -0400

    Created transaction abstract class

---
+++ CashIQ/Models/Abstracts/Transaction.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashIQ.Models.Abstracts
+{
+	public abstract class Transaction
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; } = string.Empty;
+		public string? Description { get; set; } = null;
+		[Required]
+		public double Price { get; set; } = 0.00D;
+		[Required]
+		public Frequency Frequency { get; set; } = Frequency.Monthly;
+		[Required, Display(Name = "Created at")]
+		public DateTime Created_at { get; set; } = DateTime.Now;
+		[Display(Name = "Updated at")]
+		public DateTime? Updated_at { get; set; } = null;
+	}
+
+	public enum Frequency
+	{
+		[Display(Name = "Daily")]
+		Daily,
+		[Display(Name = "Weekly")]
+		Weekly,
+		[Display(Name = "Bi-Weekly")]
+		BiWeekly,
+		[Display(Name = "Monthly")]
+		Monthly,
+		[Display(Name = "Quarterly")]
+		Quarterly,
+		[Display(Name = "Semi-Anually")]
+		SemiAnually,
+		[Display(Name = "Yearly")]
+		Yearly
+	}
+}
