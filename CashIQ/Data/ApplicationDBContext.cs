e3b71d0fed42ce7e0259a5fd9d68dabe502b7c4f
Hashem Abou Ahmad <hashemaa.ca@gmail.com>
2025-10-04 22:52:50 -0400

    Create “data” folder, create ApplicationDbContext and inherit DbContext

---
+++ CashIQ/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CashIQ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashIQ.Data
+{
+	public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : DbContext(options)
+	{
+		public DbSet<Income> Incomes { get; set; }
+		public DbSet<Expense> Expenses { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
