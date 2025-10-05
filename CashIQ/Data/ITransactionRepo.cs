52bdf11279d9d0f5ed0d3784d1d205152d4f4c31
Hashem Abou Ahmad <hashemaa.ca@gmail.com>
2025-10-05 13:25:40 -0400

    Created an interface and implementation for the base class Transaction that income and expense models inherit.

---
+++ CashIQ/Data/ITransactionRepo.cs
@@ -0,0 +1,15 @@
+﻿using CashIQ.Models;
+using CashIQ.Models.Abstracts;
+
+namespace CashIQ.Data
+{
+	public interface ITransactionRepo
+	{
+		public bool SaveChanges();
+		public void CreateTransaction<T>(T transactionEntity) where T : Transaction;
+		public IQueryable<T> GetAllTransactions<T>() where T : Transaction;
+		public void UpdateTransaction<T>(T transactionEntity) where T : Transaction;
+		public void DeleteTransaction<T>(T transactionEntity) where T : Transaction;
+		public T? GetTransactionById<T>(int id) where T : Transaction;
+	}
+}
---
+++ CashIQ/Data/TransactionRepo.cs
@@ -0,0 +1,45 @@
+﻿using CashIQ.Models.Abstracts;
+
+namespace CashIQ.Data
+{
+	public class TransactionRepo(ApplicationDBContext context) : ITransactionRepo
+	{
+		private readonly ApplicationDBContext _context = context;
+
+		public void CreateTransaction<T>(T transactionEntity) where T : Transaction
+		{
+			ArgumentNullException.ThrowIfNull(transactionEntity);
+
+			_context.Add(transactionEntity);
+		}
+
+		public void DeleteTransaction<T>(T transactionEntity) where T : Transaction
+		{
+			ArgumentNullException.ThrowIfNull(transactionEntity);
+
+			_context.Remove(transactionEntity);
+		}
+
+		public IQueryable<T> GetAllTransactions<T>() where T : Transaction
+		{
+			return _context.Set<T>();
+		}
+
+		public T? GetTransactionById<T>(int id) where T : Transaction
+		{
+			return _context.Find<T>(id);
+		}
+
+		public bool SaveChanges()
+		{
+			return _context.SaveChanges() > 0;
+		}
+
+		public void UpdateTransaction<T>(T transactionEntity) where T : Transaction
+		{
+			ArgumentNullException.ThrowIfNull(transactionEntity);
+
+			_context.Update(transactionEntity);
+		}
+	}
+}
