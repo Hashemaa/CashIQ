c5e724b143acebb134b7c729123a841d38f2a227
Hashem Abou Ahmad <hashemaa.ca@gmail.com>
2025-10-05 13:46:46 -0400

    Create API controller and inject interface dependency into controller’s constructor

---
+++ CashIQ/Controllers/Api/DashboardApiController.cs
@@ -0,0 +1,13 @@
+﻿using CashIQ.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashIQ.Controllers.Api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DashboardApiController(ITransactionRepo transactionRepo) : ControllerBase
+	{
+		private readonly ITransactionRepo _transactionRepo = transactionRepo;
+	}
+}
